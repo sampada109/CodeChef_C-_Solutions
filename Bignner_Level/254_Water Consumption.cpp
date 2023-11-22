11abeb4b3444b9582c961202c2cb13170e219eaf
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-22 13:52:42 +0530

    Create 254_Water Consumption.cpp

---
+++ Bignner_Level/254_Water Consumption.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void result(int n){
+    if(n>=2000) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int n;
+	    cin>>n;
+	    result(n);
+	}
+	return 0;
+}
