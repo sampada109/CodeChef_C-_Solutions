cf01ffd0999c9e06c6ac7de0448f5a465944d107
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-22 14:10:53 +0530

    Create 260_Chef Plays Ludo.cpp

---
+++ Bignner_Level/260_Chef Plays Ludo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	int n;
+	cin>>n;
+	if(n==6) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	}
+	return 0;
+}
