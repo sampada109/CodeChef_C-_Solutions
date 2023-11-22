b4f37e0002b3f95afdc5f849fb0f288a2c102812
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-22 13:37:42 +0530

    Create 242_Add Two Numbers.cpp

---
+++ Bignner_Level/242_Add Two Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void result(int a,int b){
+    int r = a+b;
+    cout<<r<<endl;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    result(a,b);
+	}
+	return 0;
+}
