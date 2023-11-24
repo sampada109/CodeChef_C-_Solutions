78239f6eedc0a72b95b118d33767f43a461688d4
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-24 20:56:03 +0530

    Create 273_IPL Ticket Rush.cpp

---
+++ Bignner_Level/273_IPL Ticket Rush.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b) cout<<a-b<<endl;
+	    else cout<<0<<endl;
+	}
+	return 0;
+}
