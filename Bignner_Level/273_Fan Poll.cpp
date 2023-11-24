ee6e4e5b39b5cef20a96bdd51516e7d628762f8c
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-24 20:58:14 +0530

    Create 273_Fan Poll.cpp

---
+++ Bignner_Level/273_Fan Poll.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int d,v,r;
+	cin>>d>>v>>r;
+	if (d>v and d>r) cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+	return 0;
+}
