f61c053252df88946de04b501990afa7e869777e
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-25 15:32:54 +0530

    Create 280_Bone Appetit.cpp

---
+++ Bignner_Level/280_Bone Appetit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	int c,d;
+	cin>>c>>d;
+	int r = (a*c)+(b*d);
+	cout<<r<<endl;
+	return 0;
+}
