29ecd13b71674950ec0cf7fb4ce7d375b3bb0f82
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-25 15:54:28 +0530

    Create 281_Who is taller!.cpp

---
+++ Bignner_Level/281_Who is taller!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	   int a,b;
+	   cin>>a>>b;
+	   if(a>b) cout<<"A"<<endl;
+	   else cout<<"B"<<endl;
+	    
+	}
+	return 0;
+}
