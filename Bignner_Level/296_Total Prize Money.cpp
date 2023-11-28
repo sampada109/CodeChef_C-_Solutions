63a3079c6af21781179802a11d18769c4e942900
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-28 21:25:42 +0530

    Create 296_Total Prize Money.cpp

---
+++ Bignner_Level/296_Total Prize Money.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    int r = (a*10)+(b*90);
+	    cout<<r<<endl;
+	}
+	return 0;
+}
