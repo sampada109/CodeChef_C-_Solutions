ff0d0d8fa96dfddfe8936574eec4ad05fd6ccaf5
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-28 21:23:35 +0530

    Create 295_Parity.cpp

---
+++ Bignner_Level/295_Parity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int n;
+	    cin>>n;
+	    if(n%2==0) cout<<"Yes"<<endl;
+	    else cout<<"No"<<endl;
+	}
+	return 0;
+}
