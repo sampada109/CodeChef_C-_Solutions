bd5806d8e34524943bf9e8d47dc3954dc702e453
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-28 21:43:19 +0530

    Create 298_Tom and Jerry Chase.cpp

---
+++ Bignner_Level/298_Tom and Jerry Chase.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<b) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
