c7db926f66451765c76f23460ef551a0d40c27d0
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-29 21:49:41 +0530

    Create 299_Ageing.cpp

---
+++ Bignner_Level/299_Ageing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int n;
+	    cin>>n;
+	    int r = n-10;
+	    cout<<r<<endl;
+	}
+	return 0;
+}
