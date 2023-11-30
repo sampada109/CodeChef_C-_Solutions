f04daefeeb6791c11002bc56f3fdb2d0d1566d9a
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-11-30 21:03:59 +0530

    Create 306_Number of Credits.cpp

---
+++ Bignner_Level/306_Number of Credits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int r = (a*4)+(b*2);
+	    cout<<r<<endl;
+	}
+	return 0;
+}
