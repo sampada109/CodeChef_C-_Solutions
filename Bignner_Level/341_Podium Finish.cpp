86205ba0f0c895858861ae6e064cea8a6f5ff176
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-05 21:44:07 +0530

    Create 341_Podium Finish.cpp

---
+++ Bignner_Level/341_Podium Finish.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    int r = a+b;
+	    cout<<r<<endl;
+	}
+	return 0;
+}
