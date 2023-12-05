95f5070b6d3865c0f35bd7c333f9c382c15017de
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-05 21:54:05 +0530

    Create 247_Couple Game.cpp

---
+++ Bignner_Level/247_Couple Game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b;
+	    cin>>a>>b;
+	    int r = b-a;
+	    cout<<r<<endl;
+	}
+	return 0;
+}
