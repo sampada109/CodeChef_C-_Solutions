99ae44ad20a32e9fafd594f1e3c194e1509d163e
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-12 18:56:17 +0530

    Create 348_Sleep deprivation.cpp

---
+++ Bignner_Level/348_Sleep deprivation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int n;
+	    cin>>n;
+	    if(n<7) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+}
