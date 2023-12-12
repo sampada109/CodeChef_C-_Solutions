626c8cc224c0cd3c399ba68ddae0d695de2afc8d
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-12 19:03:47 +0530

    Create 350_Chef and Chapters.cpp

---
+++ Bignner_Level/350_Chef and Chapters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<a*b*c<<endl;
+	}
+}
