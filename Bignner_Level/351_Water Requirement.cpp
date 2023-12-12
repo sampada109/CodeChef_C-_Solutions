9143af479d353eb1e78ce1760575d3404080d03e
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-12 19:35:45 +0530

    Create 351_Water Requirement.cpp

---
+++ Bignner_Level/351_Water Requirement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int k=0; k<t; k++){
+	    int n;
+	    cin>>n;
+	    cout<<2*n<<endl;
+	}
+}
