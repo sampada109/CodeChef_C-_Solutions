1e75aa4608edba750449f0dbf82ea0915330ad0b
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-12-12 19:45:48 +0530

    Create 355_Apples and Oranges.cpp

---
+++ Bignner_Level/355_Apples and Oranges.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int m;
+	cin>>m;
+	int o,a;
+	cin>>o>>a;
+	if(m>=(o+a)) cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+}
