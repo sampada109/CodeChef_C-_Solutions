3b2e13b20f4a6d301b7145c6a477fb0c43ab3bbf
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-16 23:23:44 +0530

    Create 362_Alice and Marks.cpp

---
+++ Bignner_Level/362_Alice and Marks.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+	// your code goes here
+	
+	if(x >= 2*y) cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+	return 0;
+}
