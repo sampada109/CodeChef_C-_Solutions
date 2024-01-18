7c65ed9e0a154f6654789154047b829d55e0abfe
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-18 21:24:52 +0530

    Create 374_Car Trip.cpp

---
+++ Bignner_Level/374_Car Trip.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int n;
+	    cin>>n;
+	    
+	    if(n>300) cout<<(n*10)<<endl;
+	    else cout<<3000<<endl;
+	}
+
+}
