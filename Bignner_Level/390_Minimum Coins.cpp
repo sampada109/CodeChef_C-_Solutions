98c9879efa8e278f5737027f139989ec95ba7e6e
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-19 19:33:50 +0530

    Create 390_Minimum Coins.cpp

---
+++ Bignner_Level/390_Minimum Coins.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	for(int i; i<t; i++){
+	    int n;
+	    cin>>n;
+	    if(n>=10) cout<<n%10<<endl;
+	    else cout<<n<<endl;
+	}
+
+}
