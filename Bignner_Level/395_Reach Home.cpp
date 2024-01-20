0aad1a500a0c18cb5adfc21831f03cea0fc409ab
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-20 19:48:57 +0530

    Create 395_Reach Home.cpp

---
+++ Bignner_Level/395_Reach Home.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int p,d;
+	    cin>>p>>d;
+	    if((p*5)>=d) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
