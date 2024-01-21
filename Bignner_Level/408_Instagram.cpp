afebbc6cb218ef8b484b0b6242ffa31b6ab79183
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-21 23:21:48 +0530

    Create 408_Instagram.cpp

---
+++ Bignner_Level/408_Instagram.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>(b*10)) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
