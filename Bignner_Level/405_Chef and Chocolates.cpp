a67e4b230d66a910041bdf52d2fed4f827478ab7
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-21 23:16:57 +0530

    Create 405_Chef and Chocolates.cpp

---
+++ Bignner_Level/405_Chef and Chocolates.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        // your code goes here
+        int total = (x*5) + (y*10);
+        cout<<total/z<<endl;
+    }
+	
+
+}
