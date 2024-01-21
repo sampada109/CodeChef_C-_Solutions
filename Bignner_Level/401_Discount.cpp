48450d8f993d3bd086e7b3df26afe64a88a38b13
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-21 23:14:24 +0530

    Create 401_Discount.cpp

---
+++ Bignner_Level/401_Discount.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int n;
+	    cin>>n;
+	    cout<<100-n<<endl;
+	}
+
+}
