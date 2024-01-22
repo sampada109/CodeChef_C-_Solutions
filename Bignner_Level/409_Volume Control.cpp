34aadb89e50ea1e63dc166b89861d3813e86857e
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-22 20:13:58 +0530

    Create 409_Volume Control.cpp

---
+++ Bignner_Level/409_Volume Control.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<abs(a-b)<<endl;
+	}
+
+}
