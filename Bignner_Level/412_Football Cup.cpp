919b89c5b2f02839cd612160ad70abed87d928b2
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-22 20:33:49 +0530

    Create 412_Football Cup.cpp

---
+++ Bignner_Level/412_Football Cup.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    if(a==b && a>0 && b>0) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
