2559610bca2732da32b7faea72b797176c6e208a
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-23 19:38:45 +0530

    Create 419_Parliament.cpp

---
+++ Bignner_Level/419_Parliament.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b;
+	    cin>>a>>b;
+	    if(b*2>=a) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
