d57cb7432d6f9f97d8eb532fc774e085296a708f
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-24 19:04:29 +0530

    Create 429_Candy Store.cpp

---
+++ Bignner_Level/429_Candy Store.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        // your code goes here
+        if(y>x) cout<<x+2*(y-x)<<endl;
+        else cout<<y<<endl;
+    }
+	
+
+}
