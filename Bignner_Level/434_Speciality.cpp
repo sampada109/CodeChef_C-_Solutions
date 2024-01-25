d4c595ec9118634dbc01b7a8fef2f0f2efa5fd63
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-25 22:01:45 +0530

    Create 434_Speciality.cpp

---
+++ Bignner_Level/434_Speciality.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    
+	    if(a>b && a>c) cout<<"Setter"<<endl;
+	    else if(b>c) cout<<"Tester"<<endl;
+	    else cout<<"Editorialist"<<endl;
+	}
+
+}
