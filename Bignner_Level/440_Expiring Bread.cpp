2418e69fb9a7b3e002183ec27c9fcabe99ceff6f
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-26 22:45:18 +0530

    Create 440_Expiring Bread.cpp

---
+++ Bignner_Level/440_Expiring Bread.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if((b*c)>=a) cout<<"Yes"<<endl;
+	    else cout<<"No"<<endl;
+	}
+
+}
