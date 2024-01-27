42aee835be8037ba50b8920732d550910f073897
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-27 21:32:36 +0530

    Create 452_	 Tyre problem.cpp

---
+++ Bignner_Level/452_	 Tyre problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int a,b;
+	    cin>>a>>b;
+	    int res = (a*2) + (b*4);
+	    cout<<res<<endl;
+	}
+
+}
