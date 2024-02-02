0e32a1fe29326da911c71ce93800192237ad239e
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-02 21:36:44 +0530

    Create 479_Battery Low.cpp

---
+++ Bignner_Level/479_Battery Low.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int n;
+	    cin>>n;
+	    
+	    if(n>15) cout<<"No"<<endl;
+	    else cout<<"Yes"<<endl;
+	}
+
+}
