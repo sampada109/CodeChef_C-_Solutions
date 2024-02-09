c36d4f1027081360cc8abc034b295a024511d9db
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-09 21:38:03 +0530

    Create 492_Chef and Chocolates.cpp

---
+++ Bignner_Level/492_Chef and Chocolates.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int tt,l,c;
+	    cin>>tt>>l>>c;
+	    int res = (tt-l)*c;
+	    cout<<res<<endl;
+	}
+}
