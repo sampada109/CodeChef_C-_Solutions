9b25a986eb332055f2c02b71ae0700d80708b631
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-09 21:35:20 +0530

    Create 489_Playlist.cpp

---
+++ Bignner_Level/489_Playlist.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int j,m;
+	    cin>>j>>m;
+	    int res = j/(3*m);
+	    cout<<res<<endl;
+	}
+}
