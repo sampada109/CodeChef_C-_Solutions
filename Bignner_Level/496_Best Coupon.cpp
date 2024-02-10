f976472b3b36b9a5e2ba737f7e43fba13908ed37
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-10 23:06:59 +0530

    Create 496_Best Coupon.cpp

---
+++ Bignner_Level/496_Best Coupon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int n;
+	    cin>>n;
+	    int dis = n*0.1;
+	    if(100>dis) cout<<100<<endl;
+	    else cout<<dis<<endl;
+	}
+
+}
