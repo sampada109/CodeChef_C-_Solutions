21619cb47728e8d56bcc3622e6849ce4cbbe5db9
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-13 22:50:12 +0530

    Create 505_Cricket Match.cpp

---
+++ Bignner_Level/505_Cricket Match.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int r,o;
+	    cin>>r>>o;
+	    
+	    if((36*o)>=r) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
