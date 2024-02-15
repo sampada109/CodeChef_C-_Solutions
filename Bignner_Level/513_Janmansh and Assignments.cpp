cbc94cdb9cc9a10e296953245919e2dad581b982
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-15 23:09:42 +0530

    Create 513_Janmansh and Assignments.cpp

---
+++ Bignner_Level/513_Janmansh and Assignments.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++){
+	    int n;
+	    cin>>n;
+	    
+	    if((10-n)>=3) cout<<"Yes"<<endl;
+	    else cout<<"No"<<endl;
+	}
+
+}
