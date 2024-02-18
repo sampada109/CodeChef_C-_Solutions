365f70ea1bf13e65aaf23713756fda8cd48cd09e
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-18 22:42:46 +0530

    Create 453_Judged.cpp

---
+++ Bignner_Level/453_Judged.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int a , b , c , d , e;
+	    cin>>a>> b >> c >> d >> e;
+	    if(a+b+c+d+e >= 4) cout << "YES"<<endl;
+	    else cout << "NO"<<endl;
+	}
+}
