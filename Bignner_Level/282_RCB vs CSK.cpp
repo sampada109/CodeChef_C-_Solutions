06aabf5816dca5c5793d6e74b6b7f049115c2b08
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 17:30:01 +0530

    Create 282_RCB vs CSK.cpp

---
+++ Bignner_Level/282_RCB vs CSK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int r,c;
+	
+	cin>> r >> c;
+	
+	if( (r-c) >= 18 ){
+	    cout<<"RCB";
+	}
+	else{
+	    cout<<"CSK";
+	}
+}
