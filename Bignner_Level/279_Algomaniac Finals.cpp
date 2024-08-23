4c52f92c81adaf07d2490ef7d75ead57291d25fb
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 17:20:17 +0530

    Create 279_Algomaniac Finals.cpp

---
+++ Bignner_Level/279_Algomaniac Finals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n == 17 ){
+	    cout<<"YAY";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
