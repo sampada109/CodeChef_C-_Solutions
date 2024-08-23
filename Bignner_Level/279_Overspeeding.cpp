66d9df6eebb63fb095a64d05c7dcd82a709cb297
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 17:23:56 +0530

    Create 279_Overspeeding.cpp

---
+++ Bignner_Level/279_Overspeeding.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n <= 40 ){
+	    cout<<"NO";
+	}
+	else{
+	    cout<<"YES";
+	}
+
+}
