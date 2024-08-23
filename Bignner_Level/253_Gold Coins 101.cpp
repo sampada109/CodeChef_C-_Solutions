24bae34cca6bb62df8274caa86d787852b868bbc
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 17:02:07 +0530

    Create 253_Gold Coins 101.cpp

---
+++ Bignner_Level/253_Gold Coins 101.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a,b,x,y;
+	
+	cin>> a >> b >> x >> y;
+	
+	if( x > y ){
+	    cout<<a;
+	}
+	else{
+	    cout<<b;
+	}
+
+}
