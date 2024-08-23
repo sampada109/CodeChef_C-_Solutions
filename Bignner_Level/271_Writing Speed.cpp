566cd5d61e325b12b32b7483163ea394c43e7003
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 17:16:31 +0530

    Create 271_Writing Speed.cpp

---
+++ Bignner_Level/271_Writing Speed.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n*5 <= 60 ){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
