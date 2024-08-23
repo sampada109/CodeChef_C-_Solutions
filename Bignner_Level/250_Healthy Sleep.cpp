30319987937d1fa1328e0ca81e3f9e4436170c90
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 16:54:09 +0530

    Create 250_Healthy Sleep.cpp

---
+++ Bignner_Level/250_Healthy Sleep.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n > 8 ){
+	    cout<<"MORE";
+	}
+	else if( n == 8 ){
+	    cout<<"PERFECT";
+	}
+	else{
+	    cout<<"LESS";
+	}
+
+}
