c7bf206f059b4b5e025db7105d36a4ffaf5c0a59
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-23 16:42:29 +0530

    Create 242_Summer Time.cpp

---
+++ Bignner_Level/242_Summer Time.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n > 35 ){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
