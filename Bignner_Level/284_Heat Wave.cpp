95b2b3a0e83736eb9584020a188ecc4aa5826291
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 16:47:28 +0530

    Create 284_Heat Wave.cpp

---
+++ Bignner_Level/284_Heat Wave.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int p, n;
+	
+	cin>>p>>n;
+	
+	if( p >= n ){
+	    cout<<"NO";
+	}
+	else{
+	    cout<<"YES";
+	}
+}
