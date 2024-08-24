44160dedfca1b657da4aee22dca2955a9f9c5201
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 19:22:01 +0530

    Create 354_ICE CREAM.cpp

---
+++ Bignner_Level/354_ICE CREAM.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int c, m ;
+	
+	cin >> c >> m;
+	
+	if( (2*c) <= m ){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
