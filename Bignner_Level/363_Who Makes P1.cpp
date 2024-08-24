a902c0036c722ed7396def61f1cfd15f6b94d07a
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 21:47:59 +0530

    Create 363_Who Makes P1.cpp

---
+++ Bignner_Level/363_Who Makes P1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t, d;
+	
+	cin >> t >> d;
+	
+	if( t <= d ){
+	    cout<<"Tyro";
+	}
+	else{
+	    cout<<"Dom";
+	}
+
+}
