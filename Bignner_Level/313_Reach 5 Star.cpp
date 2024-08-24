42d3b6f2ea868778e6d85c7dc440020766ef99eb
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 18:51:28 +0530

    Create 313_Reach 5 Star.cpp

---
+++ Bignner_Level/313_Reach 5 Star.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int c , n;
+	
+	cin >> c >> n;
+	
+	int tt = c + n;
+	
+	if( tt >= 2000 ){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
