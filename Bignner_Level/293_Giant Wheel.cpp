cf029ccd782ff8d8901d1b59988cb5d59c33a458
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 18:31:21 +0530

    Create 293_Giant Wheel.cpp

---
+++ Bignner_Level/293_Giant Wheel.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin>>n;
+	
+	if( n >= 60 ){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+
+}
