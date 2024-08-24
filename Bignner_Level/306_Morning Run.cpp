88dd4eb4c957f0d8ae77dabab0de32bf686433f7
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-08-24 18:37:22 +0530

    Create 306_Morning Run.cpp

---
+++ Bignner_Level/306_Morning Run.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int w , h;
+	
+	cin >> w >> h;
+	
+	int tt = (2*w) + (2*h);
+	
+	if( tt >= 1000 ){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
