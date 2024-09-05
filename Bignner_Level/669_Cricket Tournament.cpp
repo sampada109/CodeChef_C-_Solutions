ef463880399f3d6906fdd309d1fb4697fd690b5a
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-09-05 21:50:39 +0530

    Create 669_Cricket Tournament.cpp

---
+++ Bignner_Level/669_Cricket Tournament.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int teams, matches;
+	    cin >> teams >> matches;
+	    
+	    if( teams > matches ){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
