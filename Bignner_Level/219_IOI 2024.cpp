72f85daf7e1606eac621c6a391b74e6f19e75096
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-09-05 21:05:46 +0530

    Create 219_IOI 2024.cpp

---
+++ Bignner_Level/219_IOI 2024.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	
+	cin >> n;
+	
+	if( n>=1 && n<=8 ){
+	    cout << "YES" << endl; 
+	}
+	else{
+	    cout << "NO" << endl;
+	}
+
+}
